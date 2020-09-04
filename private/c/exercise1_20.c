c3e034d0af6cadfb3b57b2c7ecee70bbae033cc6
notnulldev <notnulldev@gmail.com>
2020-09-04 14:02:17 +0200

    done exercise 1.20

--- private/c/exercise1_20.c
+++ private/c/exercise1_20.c
@@ -36,12 +36,13 @@ int getUserInput(char userInput[])
 }
 int detab(char input[], int inputLength, int tab_every)
 {
-    int amountOfTabs, textBeforeTabLength, textAfterTabLength, firstTabIndex, tabsPositions[MAXLINE / TAB_LENGTH], newArrayLength, beforeIndex, afterIndex, currentArrayLength;
+    int amountOfTabs, textBeforeTabLength, currentInputLength, previousArrayLength, textAfterTabLength, firstTabIndex, currentTabPosition, tabsPositions[MAXLINE / TAB_LENGTH], newArrayLength, beforeIndex, afterIndex, currentArrayLength;
     char textBeforeTab[MAXLINE], textAfterTab[MAXLINE], newArray[MAXLINE];
 
     amountOfTabs = inputLength / tab_every;
     firstTabIndex = tab_every - 1;
     newArrayLength = inputLength + 3 * amountOfTabs;
+    // caluculate index positions based on amounts of tabs and save them in array
     for (int i = 0; i < amountOfTabs; ++i)
     {
         tabsPositions[i] = firstTabIndex + 8 * i;
@@ -49,21 +50,40 @@ int detab(char input[], int inputLength, int tab_every)
 
     for (int i = 0; i < amountOfTabs; ++i)
     {
-        textBeforeTabLength = tabsPositions[i] - 1;
-        textAfterTabLength = inputLength - (textBeforeTabLength + TAB_LENGTH);
-        currentArrayLength = textBeforeTabLength + TAB_LENGTH * (i + 1) + textAfterTabLength;
-
-        beforeIndex = 0;
-        afterIndex = newArrayLength - tabsPositions[i] + TAB_LENGTH - 1;
+        //get essencial current data for each literation
+        currentArrayLength = inputLength + 3 * (i + 1);
+        previousArrayLength = inputLength + 3 * i;
+        currentTabPosition = tabsPositions[i];
+        textBeforeTabLength = currentTabPosition;
+        textAfterTabLength = previousArrayLength - textBeforeTabLength - 1;
+        afterIndex = 0;
+        currentInputLength = 0;
+        //clear old before and after arrays
+        for (int j = 0; j < MAXLINE; j++)
+        {
+            textBeforeTab[i] = '\0';
+            textAfterTab[i] = '\0';
+        }
+        //feel the array (before and after)
+        for (int j = 0; j < previousArrayLength; j++)
+        {
+            if (j < textBeforeTabLength)
+                textBeforeTab[j] = input[j];
+            else if (j > textBeforeTabLength)
+                textAfterTab[afterIndex++] = input[j];
+        }
+        afterIndex = 0;
+        //feel input with correct data
         for (int j = 0; j < currentArrayLength; ++j)
         {
-            if (j <= tabsPositions[i])
-                continue;
-            if (j >= tabsPositions[i] + TAB_LENGTH)
-                input[j] = input[afterIndex + j];
+            if (j < textBeforeTabLength)
+                input[j] = textBeforeTab[j];
+            else if (j >= textBeforeTabLength + TAB_LENGTH)
+                input[j] = textAfterTab[afterIndex++];
             else
                 input[j] = ' ';
         }
     }
+
     return newArrayLength;
 }
