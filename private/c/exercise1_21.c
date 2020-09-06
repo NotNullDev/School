cfa6a98ee51b9727de88bc5f38700ef93e9444d9
notnulldev <notnulldev@gmail.com>
2020-09-06 22:19:54 +0200

    still ex 1 21

--- private/c/exercise1_21.c
+++ private/c/exercise1_21.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 #define MAXLINE 100
+#define TAB_LENGTH 4
 
 int getUserInput(char[]);
 void detab(char[], int, int);
+int getAmountOfNextSpaces(char input[], int inputLen, int currentIndex);
 
 int main(void)
 {
@@ -31,4 +33,62 @@ int getUserInput(char userInput[])
     userInput[length++] = '\0';
 
     return length;
+}
+
+void detab(char input[], int inputLen, int maxline)
+{
+    for (int i = 0; i < inputLen; ++i)
+    {
+        int space_index;
+        int scpaces_len;
+
+        char before[maxline];
+        char after[maxline];
+        char newArray[maxline];
+
+        int beforeLen;
+        int afterLen;
+        int newArrayLen;
+
+        int tabs_amount;
+        int spaces_amount;
+
+        space_index = -1;
+        scpaces_len = 0;
+        if (input[i] == ' ')
+        {
+            space_index = i;
+            scpaces_len = getAmountOfNextSpaces(input, inputLen, i) + 1;
+            tabs_amount = scpaces_len / TAB_LENGTH;
+            spaces_amount = scpaces_len % TAB_LENGTH;
+            newArrayLen = inputLen - 3 * tabs_amount;
+            if (scpaces_len < 4)
+                continue;
+            for (int j = 0; j < space_index; ++j)
+                newArray[j] = input[j];
+            for (int j = space_index; j < space_index + tabs_amount; ++j)
+                newArray[j] = '\t';
+            for (int j = space_index + tabs_amount; j < space_index + tabs_amount + spaces_amount; ++j)
+                newArray[j] = input[j];
+            for (int j = space_index + tabs_amount + spaces_amount; j < inputLen - 3 * tabs_amount; ++j)
+                newArray[j] = input[j];
+            for (int j = 0; j < newArrayLen; ++j)
+                input[j] = newArray[j];
+            input[newArrayLen] = '\0';
+            inputLen = newArrayLen + 1;
+        }
+    }
+}
+int getAmountOfNextSpaces(char input[], int inputLen, int currentIndex)
+{
+    int amount;
+    amount = 0;
+    for (int i = currentIndex; i < inputLen; ++i)
+    {
+        if (input[i + 1] == ' ')
+            ++amount;
+        else
+            return amount;
+    }
+    return amount;
 }
