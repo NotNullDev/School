bd65872285f87540fc9f8658dd3a520da29cb7cb
notnulldev <notnulldev@gmail.com>
2020-09-06 16:07:23 +0200

    working on algoritm of ex1 21

--- private/c/exercise1_21.c
+++ private/c/exercise1_21.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-#define TAB_LENGTH 4
 #define MAXLINE 100
 
-void entab(char input[], int inputLength);
-int getUserInput(char userInput[]);
-int getNextSpaces(char array[], int arrayLength, int iteration);
+int getUserInput(char[]);
+void detab(char[], int, int);
 
 int main(void)
 {
     char input[MAXLINE];
     int inputLength;
+
     inputLength = getUserInput(input);
-    entab(input, inputLength);
-    //    printf("\nAfter:\n%s\n", input);
+    detab(input, inputLength, MAXLINE);
+    printf("\nInput:\n%s\n", input);
     return 0;
 }
+
 int getUserInput(char userInput[])
 {
     int length, input;
@@ -31,61 +31,4 @@ int getUserInput(char userInput[])
     userInput[length++] = '\0';
 
     return length;
-}
-void entab(char input[], int inputLength)
-{
-    // find first space in text that have next to it at least 3 spaces and save it's length
-    for (int i = 0; i < inputLength; ++i)
-    {
-        int index;
-        int length;
-
-        int amountOfTabs;
-        int amountOfSpaces;
-
-        char newArray[MAXLINE];
-        int newArrayLength;
-
-        char before[MAXLINE];
-        int beforeLength;
-
-        char after[MAXLINE];
-        int afterLength;
-
-        if (input[i] = ' ' && (length = getNextSpaces(input, inputLength, i)) >= 3)
-        {
-            ++length;
-            index = i;
-            amountOfTabs = length / TAB_LENGTH;
-            amountOfSpaces = length % TAB_LENGTH;
-            newArrayLength = beforeLength + amountOfTabs + amountOfSpaces + afterLength;
-            //get before and after
-            for (int j = 0; j < inputLength; ++j)
-            {
-                if (j < index)
-                    before[i] = input[j];
-                else if (j > index + length - 1)
-                    after[j - (index + length - 1)] = input[j];
-            }
-            //make new array
-            for (int j = 0; j < index; ++j)
-                newArray[j] = input[j];
-            //merge new array into input
-            for (int j = 0; j < newArrayLength; ++j)
-            {
-                input[j] = newArray[j];
-            }
-            inputLength = newArrayLength;
-        }
-    }
-}
-
-int getNextSpaces(char array[], int arrayLength, int iteration)
-{
-    int amount;
-    amount = 0;
-
-    for (int i = iteration + 1; array[i] == ' ' && i < arrayLength; ++i)
-        ++amount;
-    return amount;
 }
